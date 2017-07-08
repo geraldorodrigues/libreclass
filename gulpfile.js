372b73cd7c0016ad7d828461b14634fb00eeb353
Geidson Benicio Coelho <geidsonc@gmail.com>
2017-07-08 14:06:03 -0300

    Adiciona bibliotecas
    - Adiciona Yarn
    - Configura package.json
    - Configura gulpfile.js
    - Adiciona o bootstrap-styl
    - Compila bootstrap com Stylus e inclui à view master.blade.php
    - Compila bootstrap com Scripts e inclui à view master.blade.php

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+process.env.DISABLE_NOTIFIER = true;
+
+
+var elixir       = require('laravel-elixir'),
+		flex         = require('postcss-flexibility'),
+		autoprefixer = require('autoprefixer'),
+		postStylus   = require('poststylus');
+		mqpacker     = require('css-mqpacker');
+
+		require('laravel-elixir-stylus');
+elixir(function(mix) {
+
+	var configStylus = [
+		// mqpacker(),
+		// autoprefixer(),
+		// flex(),
+		// bootstrap(),
+	];
+
+	// Tarefas para o framework bootstrap
+	mix.stylus('configBootstrap.styl', 'public/assets/css/bootstrap.min.css');
+	mix.scripts('./resources/assets/lib/bootstrap/js/*.js', 'public/assets/js/bootstrap.min.js');
+
+	// mix.stylus('bootstrap.styl', 'public/css/bootstrap.min.css');
+
+	// //Libs
+	// mix.copy('bower_components/jquery/dist/jquery.min.js', 'public/lib/js');
+	//
+	// //CSS
+	// mix.copy('resources/assets/bootstrap/bootstrap.min.css', 'public/css/bootstrap.min.css');
+	//
+	// mix.version(['lib/js/jquery.min.js', 'lib/js/typed.min.js', 'js/script.min.js', 'css/bootstrap.min.css', 'css/master.min.css']);
+	//
+	// mix.browserSync({
+	// 	proxy: 'localhost:8000'
+	// });
+
+});
