343829723bdd69c296c080ca08777c8f326d0a54
Caio Ladislau <caiolaszlo@gmail.com>
2017-07-11 11:18:52 -0300

    Adiciona view utilizada para email através da landing page

---
+++ app/views/email/contact.blade.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+  <style>
+    ul li {
+      margin: 5px;
+    }
+  </style>
+</head>
+
+<body>
+  <center>
+    {{HTML::image(("http://s23.postimg.org/8imz07wyj/banner_svi.png"), null, ["style" => "width: 200px;"])}}
+
+    <p style="font-size: 16pt">{{ $data['name'] }} , seja bem vindo à nossa rede!</p>
+  </center>
+
+    <h4 style="font-size: 16pt">Recebemos a sua mensagem e responderemos o mais breve possível.</h4>
+
+    <font style="font-size: 14pt; ">
+      Informações:
+      <ul>
+          <li>Nome: {{ $data['name'] }}</li>
+          <li>Telefone: {{ $data['phone'] }}</li>
+          <li>Assunto: {{ $data['subject'] }}</li>
+          <li>Mensagem: {{ $data['message'] }}</li>
+      </ul>
+    </font>
+
+</body>
+</html>
