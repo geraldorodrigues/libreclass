a9ef5872fe426f1f65c8d05202b13920b67216dd
Thiago Bruno <thiago_brro@hotmail.com>
2017-08-04 15:24:02 -0300

    Correção de sintaxe e exclusão de métodos desnecessários no model Attend

--- app/MongoDb/Attend.php
+++ app/MongoDb/Attend.php
@@ -6,51 +6,21 @@ use Jenssegers\Mongodb\Eloquent\SoftDeletes;
 class Attend extends \Moloquent
 {
 	use SoftDeletes;
-
 	protected $fillable = ['student_id', 'unit_id'];
 
 	public function unit()
 	{
-		return $this->belongsTo('Unit', 'unit_id');
+		return $this->belongsTo('App\MongoDb\Unit', 'unit_id');
 	}
 
 	public function student()
 	{
-		return $this->belongsTo('User','student_id');
+		return $this->belongsTo('App\MongoDb\User','student_id');
 	}
 
-	public function student()
+	public function frequencies()
 	{
-		return $this->hasMany('Frequency');
+		return $this->hasMany('App\MongoDb\Frequency');
 	}
 
-	/*public function getUser()
-	{
-		return User::find($this->user_id);
-	}
-
-	public function getExamsValue($exam)
-	{
-		$examValue = ExamsValue::where("exam_id", $exam)->where("attend_id", $this->id)->first();
-		if ($examValue) {
-			return $examValue->value;
-		} else {
-			return null;
-		}
-	}
-
-	public function getDescriptiveExam($exam)
-	{
-		$examDescriptive = DescriptiveExam::where("exam_id", $exam)->where("attend_id", $this->id)->first();
-		if ($examDescriptive) {
-			return ["description" => $examDescriptive->description, "approved" => $examDescriptive->approved];
-		} else {
-			return null;
-		}
-	}
-
-	public function getUnit()
-	{
-		return Unit::find($this->idUnit);
-	}*/
 }
