93b48ba5b76f77960f16b2cba2d41caa28c18fa6
Thiago Bruno <thiago_brro@hotmail.com>
2017-08-14 15:33:37 -0300

    Retira criptografia de Course e Period

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -19,7 +19,7 @@ class CourseController extends Controller
 		}
 
 		if (isset($in->course_id)) {
-			$course = Course::find(Crypt::decrypt($in->course_id));
+			$course = Course::find($in->course_id);
 		} else {
 			$course = new Course;
 			$course->institution_id = auth()->id();
@@ -37,7 +37,7 @@ class CourseController extends Controller
 
 		$course->save();
 
-		$course->id = Crypt::encrypt($course->id);
+		$course->id = $course->id;
 
 		return ['status'=>1, 'course'=>$course];
 	}
@@ -53,7 +53,7 @@ class CourseController extends Controller
 			$course->periods = $course->periods;
 			$course->unset('created_at');
 			$course->unset('updated_at');
-			$course->id = Crypt::encrypt($course->id);
+			$course->id = $course->id;
 		}
 
 		return ['status'=>1, 'courses'=>$courses];
@@ -65,12 +65,12 @@ class CourseController extends Controller
 			return ['status'=>0, 'message'=>'Dados incompletos'];
 		}
 
-		$course = Course::find(Crypt::decrypt($in->course_id));
+		$course = Course::find($in->course_id);
 		if (!$course){
 			return ['status'=>0, 'message'=>'Curso não encontrado'];
 		}
 
-		$course->id = Crypt::encrypt($course->id);
+		$course->id = $course->id;
 
 		return ['status'=>1, 'course'=>$course];
 	}
@@ -85,7 +85,7 @@ class CourseController extends Controller
 			return ['status'=>0, 'message'=>'Dados incompletos'];
 		}
 
-		$course = Course::find(Crypt::decrypt($in->course_id));
+		$course = Course::find($in->course_id);
 		if (!$course){
 			return ['status'=>0, 'message'=>'Curso não encontrado'];
 		}
